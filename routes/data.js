35acf1878d05a4ec11c4d2eecff70a2ae70e7e3b
Ivan Tsyganok <tsyganok.ivan@gmail.com>
2019-01-27 22:02:09 +0200

    products --> basic auth added

--- routes/data.js
+++ routes/data.js
@@ -1,3 +1,7 @@
+var path = require('path');
+require('dotenv').config({path: path.join(__dirname, ".env")});
+var secret = process.env.SECRET;
+
 var express                     = require('express');
 var router                      = express.Router();
 
@@ -7,7 +11,45 @@ var pricelist_controller          = require('../controllers/pricelistController'
 var settings_controller           = require('../controllers/settingsController');
 var competitor_controller         = require('../controllers/competitorController');
 
-/// ROUTES ///
+
+// ---------------------------------------------------------
+// route middleware to authenticate and check token
+// ---------------------------------------------------------
+router.use(function(req, res, next) {
+
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.params.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+
+        // verifies secret and checks exp
+        jwt.verify(token, secret, function(err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+
+    } else {
+
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
+
+});
+
+// ---------------------------------------------------------
+// authenticated routes
+// ---------------------------------------------------------
 
 // GET data home page.
 router.get('/', product_controller.index);
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,77 @@
+var path = require('path');
+require('dotenv').config({path: path.join(__dirname, ".env")});
+
 var express = require('express');
 var router = express.Router();
+
+var jwt = require('jsonwebtoken');
+
+var User = require('../models/user');
+
+var appAdmin = process.env.APP_ADMIN;
+var appPassword = process.env.APP_PASSWORD;
+var secret = process.env.SECRET;
+
+var User = require('../models/user');
+
+router.get('/setup', function(req, res) {
+
+    // create a sample user
+    var user = new User({
+        name: appAdmin,
+        password: appPassword,
+        admin: true
+    });
+    user.save(function(err) {
+        if (err) throw err;
+
+        console.log('User saved successfully');
+        res.json({ success: true });
+    });
+});
+
+// ---------------------------------------------------------
+// authentication (no middleware necessary since this isnt authenticated)
+// ---------------------------------------------------------
+router.post('/authenticate', function(req, res) {
+
+    console.log('____Authenticate');
+    // find the user
+    User.findOne({name: req.body.name}, (err, user) => {
+
+        console.log('Found user: ' + user);
+
+        if (err) throw err;
+
+        if (!user) {
+            res.json({ success: false, message: 'Authentication failed. User not found.' });
+        } else if (user) {
+
+            // check if password matches
+            if (user.password != req.body.password) {
+                res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+            } else {
+
+                // if user is found and password is right
+                // create a token
+                var payload = {
+                    admin: user.admin
+                }
+                var token = jwt.sign(payload, secret, {
+                    expiresIn: 86400 // expires in 24 hours
+                });
+
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+
+        }
+
+    });
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
